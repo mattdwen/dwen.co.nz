699cd902f57e62619551dad7129759b4465e94a3
Matt Dwen <matt@xplore.net>
2014-08-15 11:45:07 +1200

    Prism syntax highlighting

--- app/js/main.js
+++ app/js/main.js
@@ -24,6 +24,23 @@
     // =================
     Drawer.init();
 
+    // PrismJS handler
+    // =================
+    Prism.languages.html = Prism.languages.markup;
+
+    var _prismHandler = function() {
+      $('code').not('[class*="language-"]').addClass(function() {
+        var _lang = $(this).attr('class')  || 'markup';
+
+        _lang = _lang.replace(/(language|lang)+\-/gi, '');
+        return 'language-' + (Prism.languages.hasOwnProperty(_lang) ? _lang : 'markup');
+      });
+
+      Prism.highlightAll();
+    };
+
+    _prismHandler();
+
     // GA Handler
     // =================
     var _gaHandler = function() {
