3755da6933e634b224d06c656d15d4110f6a305d
Matt Dwen <matt@group6.co.nz>
2015-06-04 20:09:21 +1200

    Copy favicon for diet

--- Gruntfile.js
+++ Gruntfile.js
@@ -247,18 +247,10 @@ module.exports = function (grunt) {
           dot: true,
           cwd: '<%= yeoman.app %>',
           src: [
-            // Jekyll processes and moves HTML and text files.
-            // Usemin moves CSS and javascript inside of Usemin blocks.
-            // Copy moves asset files and directories.
             'CNAME',
+            '*.png',
             'img/**/*',
-            'fonts/**/*',
-            // Like Jekyll, exclude files & folders prefixed with an underscore.
-            '!**/_*{,/**}'
-            // Explicitly add any files your site needs for distribution here.
-            //'_bower_components/jquery/jquery.js',
-            //'favicon.ico',
-            //'apple-touch*.png'
+            'fonts/**/*'
           ],
           dest: '<%= yeoman.dist %>'
         }, {
